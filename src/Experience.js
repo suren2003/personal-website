8a7255dab71fcb114edab5cf60031b467c287ffd
Suren Kulasegaram <surenkula24@gmail.com>
2024-11-21 21:54:10 -0500

    finishing up projects page

--- src/Experience.js
+++ src/Experience.js
@@ -1,95 +1,119 @@
-import React, { useState } from 'react';
-import './Experience.css'; // Include custom CSS
+import React, { useState } from "react";
+import "./Experience.css"; // Include custom CSS
 
 const Experience = () => {
-    // State to track which box is clicked (null means no box is selected)
-    const [selectedBox, setSelectedBox] = useState(null);
+  // State to track which box is clicked (null means no box is selected)
+  const [selectedBox, setSelectedBox] = useState(null);
 
-    // Data for your experiences with details as a list
-    const experienceData = [
-        {
-            title: "5G Baseband Software Developer Intern",
-            company: "Ericsson",
-            dates: "May 2024 - Present",
-            brief: "This role was a very big and interesting change for myself. I went from working on test automation on web development to embedded programming and wireless communications. My focus was put on developing an internal tool, baseband software, and an testing framework to emulate hardware testing.",
-            tech: "C/C++, Python, YAML, 5G NR, Agile, Git",
-            resume: (
-                <ul>
-                    <li>Developed features to enable sub-argument-driven configuration selection, simplify configuration process, and sup-
-                    port to generate multiple testcases for a Python tool that does automated YAML test case generations</li>
-                    <li>Optimized product code by refactoring complex C/C++ sections flagged by CodeScene and vigorously testing
-                    solutions to ensure code quality without breaking the current system</li>
-                </ul>
-            )
-        },
-        {
-            title: "Software Test Specialist Intern",
-            company: "Nokia",
-            dates: "Jan 2024 - April 2024",
-            brief: "This is my second term in this position with Nokia. Because of my previous term, I built up trust and was given more responsibilities. This included the migration of multiple test suites into the newly React based web app and verification of bug fixes by devs.",
-            tech: "JavaScript, Node.js, MochaJS, YAML, Postman, OpenStack, Agile, Git",
-            resume: (
-                <ul>
-                    <li>Enhanced test coverage of web app by migrating 13 test suites by redeveloping and updating test suites to be
-                    compatible with newly transitioned React based web app using JavaScript, Node.js and MochaJS</li>
-                    <li>Verified the successful implementation of bug fixes by conducting thorough manual testing across various scenarios
-                    and environments</li>
-                </ul>
-            )
-        },
-        {
-            title: "Software Test Specialist Intern",
-            company: "Nokia",
-            dates: "May 2023 - Sept 2023",
-            brief: "This was my first internship position where I gained lots of great professional experience. I mainly focused on finding and fixing issues from regression test reports.",
-            tech: "JavaScript, Node.js, MochaJS, YAML, Postman, OpenStack, Agile, Git",
-            resume: (
-                <ul>
-                    <li>Contributed to the test automation and regression testing of Nokia’s NSP web application using JavaScript, Node.js,
-                    and MochaJS</li>
-                    <li>Enhanced product quality and stability by resolving 10+ product defects identified in regression tests by utilizing
-                    debugging techniques and core web principals on the test automation code and web app</li>
-                </ul>
-            )
-        }
-    ];
+  // Data for your experiences with details as a list
+  const experienceData = [
+    {
+      title: "5G Baseband Software Developer Intern",
+      company: "Ericsson",
+      dates: "May 2024 - Present",
+      brief:
+        "This role was a very big and interesting change for myself. I went from working on test automation on web development to embedded programming and wireless communications. My focus was put on developing an internal tool, baseband software, and an testing framework to emulate hardware testing.",
+      tech: "C/C++, Python, YAML, 5G NR, Agile, Git",
+      resume: (
+        <ul>
+          <li>
+            Developed features to enable sub-argument-driven configuration
+            selection, simplify configuration process, and sup- port to generate
+            multiple testcases for a Python tool that does automated YAML test
+            case generations
+          </li>
+          <li>
+            Optimized product code by refactoring complex C/C++ sections flagged
+            by CodeScene and vigorously testing solutions to ensure code quality
+            without breaking the current system
+          </li>
+        </ul>
+      ),
+    },
+    {
+      title: "Software Test Specialist Intern",
+      company: "Nokia",
+      dates: "Jan 2024 - April 2024",
+      brief:
+        "This is my second term in this position with Nokia. Because of my previous term, I built up trust and was given more responsibilities. This included the migration of multiple test suites into the newly React based web app and verification of bug fixes by devs.",
+      tech: "JavaScript, Node.js, MochaJS, YAML, Postman, OpenStack, Agile, Git",
+      resume: (
+        <ul>
+          <li>
+            Enhanced test coverage of web app by migrating 13 test suites by
+            redeveloping and updating test suites to be compatible with newly
+            transitioned React based web app using JavaScript, Node.js and
+            MochaJS
+          </li>
+          <li>
+            Verified the successful implementation of bug fixes by conducting
+            thorough manual testing across various scenarios and environments
+          </li>
+        </ul>
+      ),
+    },
+    {
+      title: "Software Test Specialist Intern",
+      company: "Nokia",
+      dates: "May 2023 - Sept 2023",
+      brief:
+        "This was my first internship position where I gained lots of great professional experience. I mainly focused on finding and fixing issues from regression test reports.",
+      tech: "JavaScript, Node.js, MochaJS, YAML, Postman, OpenStack, Agile, Git",
+      resume: (
+        <ul>
+          <li>
+            Contributed to the test automation and regression testing of Nokia’s
+            NSP web application using JavaScript, Node.js, and MochaJS
+          </li>
+          <li>
+            Enhanced product quality and stability by resolving 10+ product
+            defects identified in regression tests by utilizing debugging
+            techniques and core web principals on the test automation code and
+            web app
+          </li>
+        </ul>
+      ),
+    },
+  ];
 
-    return (
-      <div className="experience">
-        <h1 className="title">Experience</h1>
-        <div className="experience-container">
-          {experienceData.map((experience, index) => (
-            <div
-              key={index}
-              className="experience-box"
-              onClick={() => setSelectedBox(experience)} // Open the modal with selected experience details
-            >
-              <h2>{experience.title}</h2>
-              <p><b>{experience.company}</b>, {experience.dates}</p>
-            </div>
-          ))}
-        </div>
-
-        {/* Modal component that shows more details */}
-        {selectedBox && (
-          <div className="modal-overlay" onClick={() => setSelectedBox(null)}>
-            <div className="modal-content" onClick={e => e.stopPropagation()}>
-              <h2>{selectedBox.title}</h2>
-              <p><b>{selectedBox.company}</b>, {selectedBox.dates}</p>
-              <p class="brief">
-                {selectedBox.brief}
-              </p>
-              <p className="tech">
-                <span className="tech-label">Tech, Tools & Concepts: </span>
-                {selectedBox.tech}
-              </p>
-              {selectedBox.resume}  
-              <button onClick={() => setSelectedBox(null)}>Close</button>
-            </div>
+  return (
+    <div className="experience">
+      <h1 className="title">Experience</h1>
+      <div className="experience-container">
+        {experienceData.map((experience, index) => (
+          <div
+            key={index}
+            className="experience-box"
+            onClick={() => setSelectedBox(experience)} // Open the modal with selected experience details
+          >
+            <h2>{experience.title}</h2>
+            <p>
+              <b>{experience.company}</b>, {experience.dates}
+            </p>
           </div>
-        )}
+        ))}
       </div>
-    );
+
+      {/* Modal component that shows more details */}
+      {selectedBox && (
+        <div className="modal-overlay" onClick={() => setSelectedBox(null)}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <h2>{selectedBox.title}</h2>
+            <p>
+              <b>{selectedBox.company}</b>, {selectedBox.dates}
+            </p>
+            <p className="brief">{selectedBox.brief}</p>
+            <p className="tech">
+              <span className="tech-label">Tech, Tools & Concepts: </span>
+              {selectedBox.tech}
+            </p>
+            {selectedBox.resume}
+            <button onClick={() => setSelectedBox(null)}>Close</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Experience;
--- src/Projects.js
+++ src/Projects.js
@@ -1,47 +1,116 @@
-import React from 'react';
-import './Projects.css'; // Uncomment this if you have your CSS file for styles
+import React, { useState } from "react";
+import "./Projects.css"; // Include your CSS file for custom styles
 
 const Projects = () => {
-    const projects = [
-        {
-            title: "Project Title 1",
-            description: "Brief description of the project, what technologies were used, and what was accomplished.",
-            technologies: "HTML, CSS, JavaScript",
-        },
-        {
-            title: "Project Title 2",
-            description: "Brief description of the project, what technologies were used, and what was accomplished.",
-            technologies: "React, Node.js",
-        },
-        {
-            title: "Project Title 3",
-            description: "Brief description of the project, what technologies were used, and what was accomplished.",
-            technologies: "Python, Django",
-        },
-        // Add more projects as needed
-    ];
+  // State to track which project is selected
+  const [selectedProject, setSelectedProject] = useState(null);
 
-    return (
-        <div class="project-container">
-            <h2 className="title">Projects</h2>
-            <div className="projects-container">
-                {projects.map((project, index) => (
-                    <div key={index} className="project-box">
-                        <h3 className="project-title">{project.title}</h3>
-                        <p className="project-description">{project.description}</p>
-                        <p className="tech-used"><span className="tech-label">Technologies:</span> {project.technologies}</p>
-                        <button className="view-project-button" onClick={() => openModal(`project${index + 1}`)}>View Project</button>
-                    </div>
-                ))}
+  // Data for projects
+  const projects = [
+    {
+      title: "Operating System Scheduler",
+      description:
+        "This is an simplified emulation of the Linux Completely Fair Share scheduling policy developed in C. This scheduler provides a simple visual to demonstrate how the Completely Fair Scheduler works. We use a text file that we read from to simulate all the processes we have and need to work with.",
+      technologies: "C, pthreads",
+      details: (
+        <ul>
+          <li>Based on Linux Completely Fair Scheduler</li>
+          <li>Uses Producer/Consumer model using threads</li>
+          <li>
+            Threads 1 - 4 are the Consumer CPU threads, which execute the tasks
+          </li>
+          <li>
+            Thread 0 is the Producer which adds tasks to the queues of the cpus
+          </li>
+        </ul>
+      ),
+      githubURL: "https://github.com/suren2003/Operating-System-Scheduler",
+    },
+    {
+      title: "Task Management Discord Bot",
+      description:
+        "This project creates a Discord bot using Node.js to integrate Notion and Discord APIs",
+      technologies: "JavaScript, Node.js, Notion & Discord API",
+      details: (
+        <ul>
+          <li>
+            Fetches and parses data from Notion's API (calendar database).
+          </li>
+          <li>
+            Sends task notifications to a designated Discord channel using
+            Discord's API.
+          </li>
+          <li>
+            Automates weekly task updates using the node-cron library for
+            precise scheduling.
+          </li>
+        </ul>
+      ),
+      githubURL: "https://github.com/suren2003/Discord-Bot",
+    },
+    {
+      title: "Project Title 3",
+      description:
+        "A Django-based web application for managing a library. It includes user authentication and a searchable catalog.",
+      technologies: "Python, Django",
+      details: (
+        <ul>
+          <li>Created a backend system using Django</li>
+          <li>Implemented user authentication and catalog search</li>
+          <li>Integrated with a PostgreSQL database</li>
+        </ul>
+      ),
+    },
+  ];
+
+  return (
+    <div className="projects">
+      <h1 className="title">Projects</h1>
+      <div className="projects-container">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="project-box"
+            onClick={() => setSelectedProject(project)} // Open modal with selected project details
+          >
+            <h2>{project.title}</h2>
+            <p className="tech">
+              <b>Technologies:</b> {project.technologies}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      {/* Modal for project details */}
+      {selectedProject && (
+        <div className="modal-overlay" onClick={() => setSelectedProject(null)}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <h2>{selectedProject.title}</h2>
+            <p className="description">{selectedProject.description}</p>
+            <p className="tech">
+              <b>Technologies:</b> {selectedProject.technologies}
+            </p>
+            <div className="details">{selectedProject.details}</div>
+            <div className="logos-container">
+              {/* GitHub logo link */}
+              <a
+                href={selectedProject.githubURL}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={`${process.env.PUBLIC_URL}/github-logo.png`}
+                  alt="GitHub"
+                  className="logo"
+                />
+              </a>
             </div>
+            <button onClick={() => setSelectedProject(null)}>Close</button>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Projects;
-
-// Example function to handle modal opening (you can implement the modal logic as needed)
-const openModal = (projectId) => {
-    // Your logic to open the modal goes here
-    console.log(`Open modal for ${projectId}`);
-};
